a2627955dc3cfe68f66f943e71f6eee778804db2
Robin Joseph <robin@lob.com>
2018-05-12 16:06:14 -0700

    fix(nat): destroy nat gateways and eips

--- network/main.tf
+++ network/main.tf
@@ -68,26 +68,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_eip" "nat" {
-  count      = "${length(data.aws_availability_zones.available.names)}"
-  depends_on = ["aws_internet_gateway.main"]
-
-  vpc = true
-}
-
-resource "aws_nat_gateway" "main" {
-  count      = "${length(data.aws_availability_zones.available.names)}"
-  depends_on = ["aws_internet_gateway.main"]
-
-  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
-  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
-
-  tags = {
-    Name    = "${local.name}-${format("%03d", count.index + 1)}"
-    Project = "PokedexTracker"
-  }
-}
-
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.main.id}"
 
@@ -101,32 +81,6 @@ resource "aws_route" "public" {
   route_table_id         = "${aws_route_table.public.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.main.id}"
-}
-
-resource "aws_route_table" "private" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags = {
-    Name    = "${local.name}-${format("private-%03d", count.index + 1)}"
-    Project = "PokedexTracker"
-  }
-}
-
-resource "aws_route" "private" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-
-  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${element(aws_nat_gateway.main.*.id, count.index)}"
-}
-
-resource "aws_route_table_association" "private" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-
-  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
 resource "aws_route_table_association" "public" {
