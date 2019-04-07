327da12d304147554cd4c02e2d27bd297a34f9ac
Robin Joseph <robin@lob.com>
2019-04-07 14:40:07 -0700

    setup for cert-manager

--- kubernetes/outputs.tf
+++ kubernetes/outputs.tf
@@ -2,6 +2,10 @@ output "account_id" {
   value = "${data.aws_caller_identity.current.account_id}"
 }
 
+output "cert_manager_iam_role_name" {
+  value = "${aws_iam_role.cert_manager.name}"
+}
+
 output "cluster_endpoint" {
   value = "${aws_route53_record.k8s.fqdn}"
 }
